bdf383499ee8652b328d46ae45f24ceedc9f5f54
Pratik Shahi <shahipr0@gmail.com>
2021-03-19 18:57:35 +0530

    file system basic

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+//jshint esversion:6      
+//to stop warning from jshint telling it we are using newer version ES6
+
+const fs = require('fs');
+fs.copyFileSync("file1.txt","file2.txt");      //create new file and copy content of existing file to it
